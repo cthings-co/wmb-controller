8beaeed356a03abb4fcc78e433bd9efcac06c2ea
Mateusz Frasunkiewicz <m.frasunkiewicz@cthings.co>
2025-01-16 10:58:26 +0100

    Updated enums export in package and import in example.py

--- example.py
+++ example.py
@@ -1,19 +1,16 @@
 from wmb_protocol import decode_response, create_device_reset
 from wmb_protocol import print_hex_bytes
+from wmb_protocol import ModbusPort, ModbusBaud, create_baudrate_config
+
 # Print hex bytes of device reset command
 print_hex_bytes(create_device_reset())
-
-from wmb_protocol.mb_protocol_enums_pb2 import ModbusPort, ModbusBaud
-from wmb_protocol import create_baudrate_config
 
 # Print hex bytes of baudrate config command for Port 1 at 9600 baud
 print_hex_bytes(create_baudrate_config(ModbusPort.MODBUS_PORT_ONE, ModbusBaud.MODBUS_BAUD_9600))
 
 # Test data to decode
-test_data = "\x08\x47\x10\x01\x18\x01\x2a\x04\x0a\x02\x0a\x00\xef\x30"
-
-# Convert string to bytes
-frame = test_data.encode('latin1')
+test_data = b'\x08\x47\x10\x01\x18\x01\x2a\x04\x0a\x02\x0a\x00\xef\x30'
+frame = test_data  # No encoding needed
 
 # Decode the message
 success, error, message = decode_response(frame)
--- wmb_protocol/__init__.py
+++ wmb_protocol/__init__.py
@@ -9,6 +9,12 @@ from .wmb_protocol_encoder import (
     print_hex_bytes
 )
 from .wmb_protocol_decoder import decode_response
+from .mb_protocol_enums_pb2 import (
+    ModbusPort,
+    ModbusBaud,
+    ModbusMode,
+    AntennaSettings
+)
 
 __version__ = "1.0.0"
 __all__ = [
@@ -20,5 +26,9 @@ __all__ = [
     'create_modbus_oneshot',
     'create_modbus_periodic',
     'decode_response',
-    'print_hex_bytes'
+    'print_hex_bytes',
+    'ModbusPort',
+    'ModbusBaud',
+    'ModbusMode',
+    'AntennaSettings'
 ] 
