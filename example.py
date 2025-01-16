59ffea66f26c10f526a3186da68c6e692a3052b7
Mateusz Frasunkiewicz <m.frasunkiewicz@cthings.co>
2025-01-16 10:12:45 +0100

    Updated readme.md

--- example.py
+++ example.py
@@ -2,6 +2,13 @@ from wmb_protocol import decode_response, create_device_reset
 from wmb_protocol import print_hex_bytes
 # Print hex bytes of device reset command
 print_hex_bytes(create_device_reset())
+
+from wmb_protocol.mb_protocol_enums_pb2 import ModbusPort, ModbusBaud
+from wmb_protocol import create_baudrate_config
+
+# Print hex bytes of baudrate config command for Port 1 at 9600 baud
+print_hex_bytes(create_baudrate_config(ModbusPort.MODBUS_PORT_ONE, ModbusBaud.MODBUS_BAUD_9600))
+
 # Test data to decode
 test_data = "\x08\x47\x10\x01\x18\x01\x2a\x04\x0a\x02\x0a\x00\xef\x30"
 
