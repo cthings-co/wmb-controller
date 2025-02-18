e9c750c351d58e1d16335447d0824bf25f57803e
Jan Kowalewski <jkowalewski@cthings.co>
2025-02-18 15:45:45 +0100

    mb_proto: bump
    Signed-off-by: Jan Kowalewski <jkowalewski@cthings.co>

--- wmbc/mb_proto/mb_protocol_commands_pb2.py
+++ wmbc/mb_proto/mb_protocol_commands_pb2.py
@@ -25,7 +25,7 @@ _sym_db = _symbol_database.Default()
 import wmbc.mb_proto.mb_protocol_enums_pb2 as mb__protocol__enums__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1amb_protocol_commands.proto\x12\x15wmb.proto.mb_protocol\x1a\x17mb_protocol_enums.proto\"\x12\n\nEmptyFrameJ\x04\x08\x01\x10\x02\"O\n\x0f\x44\x65viceModeFrame\x12\x36\n\x0b\x64\x65vice_mode\x18\x02 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusModeJ\x04\x08\x01\x10\x02\"^\n\x14\x41ntennaSettingsFrame\x12@\n\x10\x61ntenna_settings\x18\x02 \x01(\x0e\x32&.wmb.proto.mb_protocol.AntennaSettingsJ\x04\x08\x01\x10\x02\"\xfa\x01\n\x11PortSettingsFrame\x12\x36\n\x0bmodbus_port\x18\x02 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusPort\x12\x32\n\tport_baud\x18\x03 \x01(\x0e\x32\x1f.wmb.proto.mb_protocol.PortBaud\x12\x36\n\x0bport_parity\x18\x04 \x01(\x0e\x32!.wmb.proto.mb_protocol.PortParity\x12;\n\x0eport_stop_bits\x18\x05 \x01(\x0e\x32#.wmb.proto.mb_protocol.PortStopBitsJ\x04\x08\x01\x10\x02\"h\n\x12ModbusOneShotFrame\x12\x36\n\x0bmodbus_port\x18\x02 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusPort\x12\x14\n\x0cmodbus_frame\x18\x03 \x01(\x0cJ\x04\x08\x01\x10\x02\"\x9a\x01\n\x15ModbusPeriodicalFrame\x12\x36\n\x0bmodbus_port\x18\x02 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusPort\x12\x1b\n\x13\x63onfiguration_index\x18\x03 \x01(\r\x12\x10\n\x08interval\x18\x04 \x01(\r\x12\x14\n\x0cmodbus_frame\x18\x05 \x01(\x0cJ\x04\x08\x01\x10\x02\"\xcb\x03\n\x08\x43mdFrame\x12\x38\n\x0b\x65mpty_frame\x18\x01 \x01(\x0b\x32!.wmb.proto.mb_protocol.EmptyFrameH\x00\x12\x43\n\x11\x64\x65vice_mode_frame\x18\x02 \x01(\x0b\x32&.wmb.proto.mb_protocol.DeviceModeFrameH\x00\x12M\n\x16\x61ntenna_settings_frame\x18\x03 \x01(\x0b\x32+.wmb.proto.mb_protocol.AntennaSettingsFrameH\x00\x12G\n\x13port_settings_frame\x18\x04 \x01(\x0b\x32(.wmb.proto.mb_protocol.PortSettingsFrameH\x00\x12J\n\x15modbus_one_shot_frame\x18\x05 \x01(\x0b\x32).wmb.proto.mb_protocol.ModbusOneShotFrameH\x00\x12O\n\x17modbus_periodical_frame\x18\x06 \x01(\x0b\x32,.wmb.proto.mb_protocol.ModbusPeriodicalFrameH\x00\x42\x0b\n\tcmd_frameb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1amb_protocol_commands.proto\x12\x15wmb.proto.mb_protocol\x1a\x17mb_protocol_enums.proto\"\x0c\n\nEmptyFrame\"I\n\x0f\x44\x65viceModeFrame\x12\x36\n\x0b\x64\x65vice_mode\x18\x01 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusMode\"X\n\x14\x41ntennaSettingsFrame\x12@\n\x10\x61ntenna_settings\x18\x01 \x01(\x0e\x32&.wmb.proto.mb_protocol.AntennaSettings\"\xf4\x01\n\x11PortSettingsFrame\x12\x36\n\x0bmodbus_port\x18\x01 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusPort\x12\x32\n\tport_baud\x18\x02 \x01(\x0e\x32\x1f.wmb.proto.mb_protocol.PortBaud\x12\x36\n\x0bport_parity\x18\x03 \x01(\x0e\x32!.wmb.proto.mb_protocol.PortParity\x12;\n\x0eport_stop_bits\x18\x04 \x01(\x0e\x32#.wmb.proto.mb_protocol.PortStopBits\"b\n\x12ModbusOneShotFrame\x12\x36\n\x0bmodbus_port\x18\x01 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusPort\x12\x14\n\x0cmodbus_frame\x18\x02 \x01(\x0c\"\x94\x01\n\x15ModbusPeriodicalFrame\x12\x36\n\x0bmodbus_port\x18\x01 \x01(\x0e\x32!.wmb.proto.mb_protocol.ModbusPort\x12\x1b\n\x13\x63onfiguration_index\x18\x02 \x01(\r\x12\x10\n\x08interval\x18\x03 \x01(\r\x12\x14\n\x0cmodbus_frame\x18\x04 \x01(\x0c\"\xcb\x03\n\x08\x43mdFrame\x12\x38\n\x0b\x65mpty_frame\x18\x01 \x01(\x0b\x32!.wmb.proto.mb_protocol.EmptyFrameH\x00\x12\x43\n\x11\x64\x65vice_mode_frame\x18\x02 \x01(\x0b\x32&.wmb.proto.mb_protocol.DeviceModeFrameH\x00\x12M\n\x16\x61ntenna_settings_frame\x18\x03 \x01(\x0b\x32+.wmb.proto.mb_protocol.AntennaSettingsFrameH\x00\x12G\n\x13port_settings_frame\x18\x04 \x01(\x0b\x32(.wmb.proto.mb_protocol.PortSettingsFrameH\x00\x12J\n\x15modbus_one_shot_frame\x18\x05 \x01(\x0b\x32).wmb.proto.mb_protocol.ModbusOneShotFrameH\x00\x12O\n\x17modbus_periodical_frame\x18\x06 \x01(\x0b\x32,.wmb.proto.mb_protocol.ModbusPeriodicalFrameH\x00\x42\x0b\n\tcmd_frameb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -33,17 +33,17 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'mb_protocol_commands_pb2', 
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_EMPTYFRAME']._serialized_start=78
-  _globals['_EMPTYFRAME']._serialized_end=96
-  _globals['_DEVICEMODEFRAME']._serialized_start=98
-  _globals['_DEVICEMODEFRAME']._serialized_end=177
-  _globals['_ANTENNASETTINGSFRAME']._serialized_start=179
-  _globals['_ANTENNASETTINGSFRAME']._serialized_end=273
-  _globals['_PORTSETTINGSFRAME']._serialized_start=276
-  _globals['_PORTSETTINGSFRAME']._serialized_end=526
-  _globals['_MODBUSONESHOTFRAME']._serialized_start=528
-  _globals['_MODBUSONESHOTFRAME']._serialized_end=632
-  _globals['_MODBUSPERIODICALFRAME']._serialized_start=635
-  _globals['_MODBUSPERIODICALFRAME']._serialized_end=789
-  _globals['_CMDFRAME']._serialized_start=792
-  _globals['_CMDFRAME']._serialized_end=1251
+  _globals['_EMPTYFRAME']._serialized_end=90
+  _globals['_DEVICEMODEFRAME']._serialized_start=92
+  _globals['_DEVICEMODEFRAME']._serialized_end=165
+  _globals['_ANTENNASETTINGSFRAME']._serialized_start=167
+  _globals['_ANTENNASETTINGSFRAME']._serialized_end=255
+  _globals['_PORTSETTINGSFRAME']._serialized_start=258
+  _globals['_PORTSETTINGSFRAME']._serialized_end=502
+  _globals['_MODBUSONESHOTFRAME']._serialized_start=504
+  _globals['_MODBUSONESHOTFRAME']._serialized_end=602
+  _globals['_MODBUSPERIODICALFRAME']._serialized_start=605
+  _globals['_MODBUSPERIODICALFRAME']._serialized_end=753
+  _globals['_CMDFRAME']._serialized_start=756
+  _globals['_CMDFRAME']._serialized_end=1215
 # @@protoc_insertion_point(module_scope)
