4197f3663b3083b9979e30909973a755efeca9ad
Jan Kowalewski <jkowalewski@cthings.co>
2025-03-06 13:21:31 +0100

    examples: add diagnostics example
    Signed-off-by: Jan Kowalewski <jkowalewski@cthings.co>

---
+++ examples/diagnostic.py
@@ -0,0 +1,39 @@
+import argparse
+import asyncio
+import logging
+import json
+import wmbc.mb_proto.mb_protocol_pb2 as mb_protocol
+from wmbc.mb_proto.mb_protocol_iface import MBProto
+from google.protobuf.json_format import MessageToJson
+from wmbc.wmbc import WMBController
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    parser = argparse.ArgumentParser(description='Example of running diagnostics status periodically')
+    parser.add_argument(
+        '--dst-addr',
+        required=False,
+        type=int,
+        default=2,
+        help='Wirepas destination address'
+    )
+    parser.add_argument(
+            '--period',
+            required=False,
+            type=int,
+            default=20,
+            help='Period in seconds'
+    )
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    args_dict.update({'cmd':"diag"})
+
+    WMBC = WMBController(**args_dict)
+    WMBC.initialize_sink()
+    await WMBC.run_periodically(period=args_dict.get('period'), print_default=True)
+
+if __name__ == "__main__":
+    asyncio.run(main())
