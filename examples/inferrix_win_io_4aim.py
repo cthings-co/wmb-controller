d5a649ae0f997941df49615f261f97eb04301896
Adam Szewczyk <a.szewczyk@cthings.co>
2025-03-31 13:29:43 +0200

    filtering out other frames in inferrix example
    Signed-off-by: Adam Szewczyk <a.szewczyk@cthings.co>

--- examples/inferrix_win_io_4aim.py
+++ examples/inferrix_win_io_4aim.py
@@ -11,20 +11,28 @@ from wmbc.wmbc import WMBController
 
 logging.basicConfig(level=logging.INFO)
 
-def print_answer(msg: mb_protocol.MbMessage, voltage_range: list) -> None:
+def print_answer(msg: mb_protocol.MbMessage, callback_args: dict) -> None:
+    voltage_range = callback_args.get('volt_range')
+    dev_id = callback_args.get('modbus_addr')
     _mbproto = MBProto()
     _json_str = MessageToJson(msg, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)
     _dict = json.loads(_json_str)
     if (msg.cmd == mb_protocol.Cmd.CMD_MODBUS_ONE_SHOT or msg.cmd == mb_protocol.Cmd.CMD_MODBUS_PERIODICAL):
         if (msg.payload.payload_answer_frame.ack_frame.acknowladge == 0):
             data = msg.payload.payload_answer_frame.modbus_response_frame.modbus_frame
-            modbus_frame = _mbproto.decode_modbus_frame(data)
-            _dict['payload']['payload_answer_frame']['modbus_response_frame']['modbus_frame'] = modbus_frame
+            try:
+                modbus_frame = _mbproto.decode_modbus_frame(data)
+            except Exception as e:
+                logging.error("Faild to decode Modbus Frame")
+            else:
+                _dict['payload']['payload_answer_frame']['modbus_response_frame']['modbus_frame'] = modbus_frame
     if 'modbus_response_frame' in _dict['payload']['payload_answer_frame']:
         if 'ReadHoldingRegistersResponse' in _dict['payload']['payload_answer_frame']['modbus_response_frame']['modbus_frame']:
-            ai_regs = _dict['payload']['payload_answer_frame']['modbus_response_frame']['modbus_frame']['ReadHoldingRegistersResponse']['registers']
-            for iter in range(4):
-                logging.info("AI %d is: %f V",iter+1, (ai_regs[iter]*voltage_range[iter])/(2 ** 10))
+            if _dict['payload']['payload_answer_frame']['modbus_response_frame']['modbus_frame']['ReadHoldingRegistersResponse']['dev_id'] == dev_id:
+                ai_regs = _dict['payload']['payload_answer_frame']['modbus_response_frame']['modbus_frame']['ReadHoldingRegistersResponse']['registers']
+                if len(ai_regs) >= 4:
+                    for iter in range(4):
+                        logging.info("AI %d is: %f V",iter+1, (ai_regs[iter]*voltage_range[iter])/(2 ** 10))
 
 async def main():
     parser = argparse.ArgumentParser(description='Inferix WIN-IO-4AIM demo for WMB Controller - reads AI voltage values over Wirepas \
@@ -74,10 +82,12 @@ async def main():
     read_regs_frame = generator.read_holding_registers(address=0, count=4)
 
     args_dict.update({'cmd':"modbus_1s", 'modbus_frame':read_regs_frame})
+    callback_args_dict = {}
+    callback_args_dict.update({'modbus_addr': args_dict['modbus_addr'], 'volt_range': args_dict['volt_range']})
 
     WMBC = WMBController(**args_dict)
     WMBC.initialize_sink()
-    await WMBC.run_periodically(args_dict.get('period'), _callback = print_answer, callback_args=args_dict.get('volt_range'))
+    await WMBC.run_periodically(args_dict.get('period'), _callback = print_answer, callback_args = callback_args_dict)
 
 if __name__ == "__main__":
     asyncio.run(main())
